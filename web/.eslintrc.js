8dab560eff3460fedd35578c15f3d1f51ccf2909
liujianxiong <liujianxiong@issmart.com.cn>
2020-08-16 17:55:50 +0800

    first commit

---
+++ web/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ['plugin:vue/essential', '@vue/standard'],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'generator-star-spacing': 'off',
+    'space-before-function-paren': 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
